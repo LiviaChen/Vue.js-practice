02631e79f9683b6b873e3a914581e087e4d33563
LiviaChen <66782449+LiviaChen@users.noreply.github.com>
2022-04-17 21:26:39 +0800

    adding practice files, so do thereafter

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name : 'Livia Chen',
+            age : 30,
+            enpic : 'https://image.shutterstock.com/image-vector/doodle-style-hand-drawing-business-260nw-1857850081.jpg'
+        }
+    }
+});
+
+app.mount('#assignment');
